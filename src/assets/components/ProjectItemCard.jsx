15a741ef313990ec7429063dc4da23fef422b591
Martin <mjtitus06@gmail.com>
2025-06-15 07:02:48 +0100

    Moved the projects array of objects to its own .js file. Styled the project section of the page and added a responsive layout- flexbox.

--- src/assets/components/ProjectItemCard.jsx
+++ src/assets/components/ProjectItemCard.jsx
@@ -1,27 +1,34 @@
-import link_icon from '../media/external_link_icon.png';
-import github_icon from '../media/github_black.png'
+import link_icon from "../media/external_link_icon.png";
+import github_icon from "../media/github_black.png";
 
-
-function ProjectItemCard (props) { 
-
-    return (
-        <>
-        <img src={props.projectObj.img} alt="" />
+function ProjectItemCard(props) {
+  return (
+    <section className="prjct-card">
+      <img src={props.projectObj.img} alt="project screebshot" />
+      <section className="prjct-detail-ctn">
         <h2>{props.projectObj.name}</h2>
-        <ul>
-            {props.projectObj.techUsed.map((tech) => <li>{tech}</li>)}
+        <ul className="techUsed">
+          {props.projectObj.techUsed.map((tech) => (
+            <li>{tech}</li>
+          ))}
         </ul>
-        <section>
-            <button onclick="location.href={props.projectObj.linkToRepo}" type="button">
-                <img src={github_icon} width={"20rem"}/>
-                Code
-            </button>
-            <button onclick="location.href={props.projectObj.linkToDemo}" type="button">
-                <img src={link_icon} alt="" />
-                Live Demo
-            </button>
-        </section>
-        </>
-    );
+        <p>{props.projectObj.aboutProject}</p>
+      </section>
+      <section className="prjct-btn-ctn">
+        <a href={props.projectObj.linkToRepo} target="_blank">
+          <button className="btn">
+            <img src={github_icon} width={"22rem"} />
+            Code
+          </button>
+        </a>
+        <a href={props.projectObj.linkToDemo} target="_blank">
+          <button className="btn">
+            <img src={link_icon} />
+            Live Demo
+          </button>
+        </a>
+      </section>
+    </section>
+  );
 }
 export default ProjectItemCard;
--- src/assets/components/Projects_Card.jsx
+++ src/assets/components/Projects_Card.jsx
@@ -1,38 +1,18 @@
 import ProjectItemCard from "./ProjectItemCard";
+import projectsDone from "./projects_info"
 
 function Projects_Card() {
-    const projectDone = [
-        {
-            img: "https://file.path.com",
-            name: "Project-name",
-            techUsed: ["React", "CSS", "JavaScript"],
-            aboutProject: "Short project description",
-            linkToRepo: "https://file.path.com",
-            linkToDemo: "https://file.path.com"
-        },
-        {
-            img: "https://file.path.com2",
-            name: "Project-name2",
-            techUsed: ["React", "CSS", "JavaScript"],
-            aboutProject: "Short project description2",
-            linkToRepo: "https://file.path.com2",
-            linkToDemo: "https://file.path.com2"
-        }
-    ]
+    const individualPrjctCard = projectsDone.map((project, index) => {
+    return <ProjectItemCard key={index} projectObj={project} />;
+  });
 
-    const individualPrjctCard = projectDone.map((project, index) => {
-        return <ProjectItemCard key={index} projectObj={project} />
-    });
-
-    return (
-        <div className="prjcts-container">
-            <h2>Featured Projects</h2>
-            <p>Explore some of my recent work and personal projects</p>
-            <section>
-                {individualPrjctCard}
-            </section>
-        </div>
-    );
+  return (
+    <div className="prjcts-container">
+      <h2>Featured Projects</h2>
+      <p>Explore some of my recent work and personal projects</p>
+      <section className="prjcts-card-container ">{individualPrjctCard}</section>
+    </div>
+  );
 }
 
 export default Projects_Card;
