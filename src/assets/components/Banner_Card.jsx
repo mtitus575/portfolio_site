e28b37ba7b6ba436f18bd6a306502d33b3f7fccf
Martin <mjtitus06@gmail.com>
2025-06-18 09:43:00 +0100

    Made a working contact form. Also added CV as downloadable - removed it again for now till CV ready.

--- src/assets/components/Banner_Card.jsx
+++ src/assets/components/Banner_Card.jsx
@@ -1,28 +1,43 @@
-import myImg from '../media/IMG_20220704_114658_edited.jpg'
+import myImg from "../media/IMG_20220704_114658_edited.jpg";
 
 function Banner_Card() {
   return (
-      <div className="bnr-container flex">
-        <img className='prfl-img' src={myImg} alt="Image of Martin" />
-        <section className="prfl-info-container">
-            <h1>Martin Titus</h1>
-            <h4 className='job-title'>Software Developer</h4>
-            <article className='abt-me'>
-                <p>
-                    I'm a creative and enthusiastic developer who genuinely enjoys turning ideas into fun, engaging digital experiences. 
-                    <br /><br />
-                    Learning new things and working with modern web tech keeps me excited about every project. I love teaming up with others, sharing ideas, and making sure the results are something we can all be proud of. 
-                    <br /><br />
-                    let's connect and build something great together!
-                </p>
-
-            </article>
-            <div className='bnr-btns-ctn'>
-                <button className='cv-btn btn'>Download CV</button>
-                <button className='contact-btn btn'>Contact me!</button>
-            </div>
-        </section>
-      </div>
+    <div className="bnr-container flex">
+      <img className="prfl-img" src={myImg} alt="Image of Martin" />
+      <section className="prfl-info-container">
+        <h1>Martin Titus</h1>
+        <h4 className="job-title">Software Developer</h4>
+        <article className="abt-me">
+          <p>
+            I'm a creative and enthusiastic developer who genuinely enjoys
+            turning ideas into fun, engaging digital experiences.
+            <br />
+            <br />
+            Learning new things and working with modern web tech keeps me
+            excited about every project. I love teaming up with others, sharing
+            ideas, and making sure the results are something we can all be proud
+            of.
+            <br />
+            <br />
+            let's connect and build something great together!
+          </p>
+        </article>
+        <div className="bnr-btns-ctn">
+          {/* <a
+            href="/Martin_DEV_CV.pdf" 
+            // The above file is removed from public folder for now. 
+            // BEFORE uploading it, get a private work number and change CV to not show personal details.                     
+            // Then uncomment the a tag.
+            download
+            target="_blank"
+            rel="noopener noreferrer"
+          > */}
+            <button className="cv-btn btn">Download CV</button>
+          {/* </a> */}
+          <button className="contact-btn btn">Contact me!</button>
+        </div>
+      </section>
+    </div>
   );
 }
 
--- src/assets/components/Contact_Form.jsx
+++ src/assets/components/Contact_Form.jsx
@@ -1,11 +1,11 @@
 function ContactForm() {
     return (
-        <form action="POST" className="form">
-            <input type="text" id="contactName" className="frm-style" placeholder="Your Name" required/>
-            <input type="email" id="contactEmail" className="frm-style" placeholder="Your Email" required/>
-            <input type="text" id="contactSubject" className="frm-style" placeholder="Subject" required/>
+        <form action="https://formspree.io/f/xdkzddna" method="POST" className="form">
+            <input type="text" name="name" id="contactName" className="frm-style" placeholder="Your Name" required/>
+            <input type="email" name="email" id="contactEmail" className="frm-style" placeholder="Your Email" required/>
+            <input type="text" name="subject" id="contactSubject" className="frm-style" placeholder="Subject" required/>
             <textarea name="message" id="contactUserMessage" className="frm-style" placeholder="Your Message"></textarea>
-            <input className="btn form-btn" type="submit" />
+            <input className="btn form-btn" type="submit" value="Send Message" />
         </form>
     );
 }
